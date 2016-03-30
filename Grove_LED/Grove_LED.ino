a71076c259e0fa1656f0628ba3121e51552250fb
victor-hecs <herseed@163.com>
2016-03-30 15:29:26 +0800

    Update Grove_LED.ino

--- Grove_LED/Grove_LED.ino
+++ Grove_LED/Grove_LED.ino
@@ -1,8 +1,9 @@
 // demo of Grove - Starter V2.0
 // Loovee  2013-3-10
-// as the topic, we will use Grove - Led to make a breath light
-// Grove - LED connect to D3
-// the following pin which support PWM can be used:
+// Modified by victor 2016/3/30
+// we will use Grove - LED to make a fading light
+// Connect Grove - LED  to D3
+// the following pins which support PWM can be used:
 // 3, 5, 6, 9, 10, 11
 
 const int pinLed    = 3;                        // pin of led define here
@@ -18,11 +19,11 @@ void loop()
     for(int i=0; i<256; i++)
     {
         analogWrite(pinLed, i);
-        delay(5);                               // change delay time can breath faster or slower
+        delay(5);                               // change delay time can fade more quickly  or slowly
     }
     delay(100);
     
-    for(int i=254; i>=0; i--)
+    for(int i=255; i>=0; i--)
     {
         analogWrite(pinLed, i);
         delay(5);                               // change delay time can breath faster or slower
