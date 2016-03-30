d34ccd8f7dc274eb4adbd7e38ce8004ef46b3354
victor-hecs <herseed@163.com>
2016-03-30 17:45:22 +0800

    Update Grove_Temperature_Sensor.ino

--- Grove_Temperature_Sensor/Grove_Temperature_Sensor.ino
+++ Grove_Temperature_Sensor/Grove_Temperature_Sensor.ino
@@ -1,10 +1,16 @@
-// demo of Starter Kit V2.0 - Grove Temperature Sensor
-//
+// A demo for Starter Kit V2.0 - Grove Temperature Sensor
+// 
+// Circuit:
+// Connect Grove Temperature Sensor to analog port A0;
+// 
+// Modified victor at 3/30/2016
+// www.seeedstudio.com 
+
 
 const int pinTemp = A0;      // pin of temperature sensor
 
 float temperature;
-int B=3975;                  // B value of the thermistor
+int B=3975;                  //  B constant of the thermistor, read more at https://en.wikipedia.org/wiki/Temperature_coefficient#Negative_temperature_coefficient and http://www.seeedstudio.com/wiki/Grove_-_Temperature_Sensor_V1.2
 float resistance;
 
 void setup()
@@ -15,9 +21,11 @@ void setup()
 void loop()
 {
     int val = analogRead(pinTemp);                               // get analog value
-    resistance=(float)(1023-val)*10000/val;                      // get resistance
-    temperature=1/(log(resistance/10000)/B+1/298.15)-273.15;     // calc temperature
+
+    resistance=(float)(1023-val)/val;                        // Calculate resistance
+    resistance = resistance * 10000;
+    temperature=1/(log(resistance/10000)/B+1/298.15)-273.15;     // calculate temperature
     Serial.println(temperature);
     
-    delay(1000);          // delay 1s
+    delay(1000);          // delay 1 second
 }
