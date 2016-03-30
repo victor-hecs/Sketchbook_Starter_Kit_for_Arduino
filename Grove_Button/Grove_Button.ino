5cea10e07ec09c8901f3d1a5ff5fbb3ceff7c952
victor-hecs <herseed@163.com>
2016-03-30 14:45:17 +0800

    Update Grove_Button.ino

--- Grove_Button/Grove_Button.ino
+++ Grove_Button/Grove_Button.ino
@@ -1,29 +1,29 @@
 // demo of Grove - Starter V2.0
 // Loovee  2013-3-10
 // this demo will show you how to use Grove - Button to control a LED
-// when the button was pressed, the led will on 
-// otherwise led off
+// when the button was pressed, the LED will light on 
+// otherwise LED will be remain off state
 // Grove - Button connect to D3
 // Grove - LED connect to D7
 
 const int pinButton = 3;                        // pin of button define here
-const int pinLed    = 7;                        // pin of led define here
+const int pinLed    = 7;                        // pin of LED define here
 
 void setup()
 {
     pinMode(pinButton, INPUT);                  // set button INPUT
-    pinMode(pinLed, OUTPUT);                    // set led OUTPUT
+    pinMode(pinLed, OUTPUT);                    // set LED OUTPUT
 }
 
 void loop()
 {
     if(digitalRead(pinButton))                     // when button is pressed
     {
-        digitalWrite(pinLed, HIGH);             // led on
+        digitalWrite(pinLed, HIGH);               // LED on
     }
     else
     {
-        digitalWrite(pinLed, LOW);
+        digitalWrite(pinLed, LOW);              //LED off
     }
     
     delay(10);
